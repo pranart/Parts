3a346c51bf49354ca1de162f3c4b04bd2732bc1c
Pranart Dechpokket <jojobarcream@gmail.com>
2023-03-23 23:40:39 +0700

    major again 2

--- Parts/Program.cs
+++ Parts/Program.cs
@@ -13,7 +13,7 @@ namespace Parts
 			// l == length
 			// s == split
 			// m == merge
-			// major again
+			// major again 2
 			if(CommandLine.Parser.Default.ParseArguments(args,options))
 			{
 				try
