ea6e6540c493a1aba6bf5d0afc4acff32bc238e9
Pranart Dechpokket <jojobarcream@gmail.com>
2023-03-24 00:56:56 +0700

    #minor

--- Parts/Program.cs
+++ Parts/Program.cs
@@ -13,7 +13,7 @@ namespace Parts
 			// l == length
 			// s == split
 			// m == merge
-			// minor 2
+			// minor 2 new
 			if(CommandLine.Parser.Default.ParseArguments(args,options))
 			{
 				try
